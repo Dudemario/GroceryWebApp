7ebeea5b126bce1ba78012fe4148986a32fdf488
Dudemario <vincentjsun@gmail.com>
2023-07-07 18:57:22 -0400

    Create cluster.js

---
+++ grocery_backend/cluster.js
@@ -0,0 +1,26 @@
+const { Cluster } = require('puppeteer-cluster');
+
+const urls = [];
+
+(async () => {
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_PAGE,
+    maxConcurrency: 100,
+    puppeteerOptions: {
+        headless: false,
+        defaultViewport: false,
+        userDataDir: "./tmp",
+    }
+  });
+
+  await cluster.task(async ({ page, data: url }) => {
+    await page.goto(url);
+  });
+  for (const url of urls) {
+    await cluster.queue(url);
+  }
+  // many more pages
+
+  //await cluster.idle();
+  //await cluster.close();
+})();
