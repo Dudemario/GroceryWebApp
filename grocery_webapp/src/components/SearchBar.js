0170f11a0ce70ed3b7c4eb89ef29c17db33bd87a
benjaminwang42 <benjaminwang42@gmail.com>
2023-07-07 20:20:08 -0400

    button edits

--- grocery_webapp/src/components/SearchBar.js
+++ grocery_webapp/src/components/SearchBar.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "../styles/SearchBar.css";
 import SearchIcon from '@mui/icons-material/Search';
 
 function SearchBar({placeholder, data}) {
+
+    const [filteredData, setFilteredData] = useState ([]);
+
+    const handleFilter = (event) => {
+      const searchWord = event.target.value
+      const newFilter = data.filter((value) => {
+        return value.title.toLowerCase().includes(searchWord.toLowerCase());
+      })
+      setFilteredData(newFilter);
+    }
   return (
     <div className="search">
       <div className="searchInputs">
-        <input type="text" placeholder={placeholder}/>
+        <input type="text" placeholder={placeholder} onChange= {handleFilter}/>
         <div className="searchIcon"> 
             <SearchIcon />
         </div>
       </div> 
+      {filteredData.length != 0 && ( 
       <div className="dataResult">
-        {data.map((value, key) => {
+        {filteredData.slice(0, 15).map((value, key) => {
           return  (
             <a className="dataItem" href={value.link} target="_blank"> 
             {" "}
@@ -20,6 +31,7 @@ function SearchBar({placeholder, data}) {
         );
         })}
       </div>
+      )}
     </div>
   )
 }
