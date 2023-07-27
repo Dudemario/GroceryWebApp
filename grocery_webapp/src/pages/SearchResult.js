04ae4b2789b43a4932e3660c63a5246374792296
Tvomato <justinli2013@gmail.com>
2023-07-26 20:20:06 -0400

    added updated csv files

--- grocery_webapp/src/pages/SearchResult.js
+++ grocery_webapp/src/pages/SearchResult.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Papa from 'papaparse';
 import "../styles/Result.css";
 
@@ -14,11 +15,10 @@ const getName = (filePath) => {
 
 const SearchResult = () => {
   const [fileData, setFileData] = useState([]);
-  const [sortOption, setSortOption] = useState("alphabetical");
+  const [sortOption, setSortOption] = useState("relevance");
 
   const handleSortChange = (event) => {
-    let { value } = event.target;
-    
+    let { value } = event.target;  
     setSortOption(value);
   }
 
@@ -30,6 +30,7 @@ const SearchResult = () => {
           Papa.parse(csvFiles[i], {
             download: true,
             header: true,
+            skipEmptyLines: true,
             complete: function (result) {
               parsedData.push({ name: getName(csvFiles[i]), data: result.data });
               resolve();
@@ -50,13 +51,9 @@ const SearchResult = () => {
     <div className='searchresult'>
       <h1>Showing Results For:</h1>
       <div className='options'>
-        <p className='optionText'>Sorting:</p>
-        <input type='radio' value="alphabetical" checked={sortOption === "alphabetical"} onChange={handleSortChange} /> <p className='optionText'>recommended</p> 
+        <p className='optionText'>Sort By:</p>
+        <input type='radio' value="relevance" checked={sortOption === "relevance"} onChange={handleSortChange} /> <p className='optionText'>relevance</p> 
         <input type='radio' value="priceLow" checked={sortOption === "priceLow"} onChange={handleSortChange}/> <p className='optionText'>price (lowest)</p>
-        <input type='radio' value="priceHigh" checked={sortOption === "priceHigh"} onChange={handleSortChange}/> <p className='optionText'>price (highest)</p>
-        <input type='radio' value="distLow" checked={sortOption === "distLow"} onChange={handleSortChange}/> <p className='optionText'>distance (nearest)</p>
-        <input type='radio' value="distHigh" checked={sortOption === "distHigh"} onChange={handleSortChange}/> <p className='optionText'>distance (furthest)</p>
-        <input type='checkbox' value='withinDistance'/> <p>only show stores within certain radius</p>
       </div>
       {fileData.map((file, index) => (
         <div key={index}>
@@ -64,16 +61,18 @@ const SearchResult = () => {
           <div className='items'>
             {file.data.map((item, idx) => (
               <div key={idx}>
-                <img src={item.img} alt={item.title} className='image' />
+                <a href={`//${item.url}`}><img src={item.img} alt={item.title} className='image' /></a>
                 <div className='info'>
                   <div><b>{item.title}</b></div>
                   <div>{item.price}</div>
+                  <button className='addButton'>Add to List</button>
                 </div>
               </div>
             ))}
           </div>
         </div>
       ))}
+      {fileData.length === 0 ? <h2>There was an error fetching results</h2> : null}
     </div>
   );
 };
