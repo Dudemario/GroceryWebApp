3ee583f4b033424be925adfe4d96ba22a99a30d7
Dudemario <vincentjsun@gmail.com>
2023-07-29 22:52:13 -0400

    Update cluster.js

--- grocery_backend/cluster.js
+++ grocery_backend/cluster.js
@@ -1,16 +1,32 @@
 const fs = require("fs");
+const fsExtra = require("fs-extra");
 const puppeteer = require('puppeteer-extra');
+const express = require("express");
 
+const PORT = process.env.PORT || 3000;
+const app = express();
 // add stealth plugin and use defaults (all evasion techniques)
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const { Cluster } = require('puppeteer-cluster');
 
 puppeteer.use(StealthPlugin());
 
-const stores = {"walmart":["https://www.walmart.ca/search?q=banana&c=10019", "div.css-qsotkn.e5icx9n1 > div > .css-1d0izcz.e1m8uw9118", 
-"div > span > div > p", "div > span > span", ".css-19q6667.e175iya62", ".css-mfluef.e1m8uw911"]};
-
-(async () => {
+const stores = {"walmart":["https://www.walmart.ca/search?q=", "div.css-qsotkn.e5icx9n1 > div > .css-1d0izcz.e1m8uw9118", 
+"div > span > div > p", "div > span > span", ".css-19q6667.e175iya62", ".css-mfluef.e1m8uw911"], 
+"metro":["https://www.metro.ca/en/online-grocery/search?filter=", "div.products-search--grid.searchOnlineResults > .default-product-tile.tile-product.item-addToCart",
+"div.content__head > a > div", "div.content__pricing > div > div > span","a > picture > img", ".product-details-link"], "loblaws":["https://www.loblaws.ca/search?search-bar=", 
+"ul.product-tile-group__list.product-tile-group__list--total-48 > li.product-tile-group__list__item", "span.product-name__item.product-name__item--name",
+"span.price__value.selling-price-list__item__price.selling-price-list__item__price--now-price__value", "div.product-tile__thumbnail__image > img", 
+"h3 > a"]};
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+app.post("/scrape", (req, res) => {
+  scrape(req.body);
+  //scrape(req.body);
+});
+async function scrape(object) {
+  fsExtra.emptyDirSync("/Users/vincent/Documents/Projects/React/GroceryWebApp/grocery_webapp/src/website_search");
   const cluster = await Cluster.launch({
     puppeteer,
     concurrency: Cluster.CONCURRENCY_PAGE,
@@ -20,8 +36,8 @@ const stores = {"walmart":["https://www.walmart.ca/search?q=banana&c=10019", "di
       headless: false,
       defaultViewport: false,
       userDataDir: "./tmp",
-      executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-      userDataDir: "/Users/vincent/Library/Application Support/Google/Chrome/Profile 2"
+      //executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+      //userDataDir: "/Users/vincent/Library/Application Support/Google/Chrome/Profile 2"
     }
   });
   cluster.on("taskerror", (err, data) => {
@@ -31,10 +47,24 @@ const stores = {"walmart":["https://www.walmart.ca/search?q=banana&c=10019", "di
     let storename = url.slice(12).split(".")[0];
     await page.setDefaultNavigationTimeout(0);
     await page.goto(url);
+    if(storename == "loblaws"){
+      await page.waitForSelector('h3');
+      await page.evaluate(() => {
+        window.scrollTo(0, window.document.body.scrollHeight/5);
+      });
+      await page.waitForTimeout(100);
+      
+    }
     const productsHandles = await page.$$(
       stores[storename][1]
     );
-  
+    fs.appendFile(
+      `/Users/vincent/Documents/Projects/React/GroceryWebApp/grocery_webapp/src/website_search/${storename}.csv`,
+      `title,price,img,url\n`,
+      function (err) {
+        if (err) throw err;
+      }
+    );
     //loop through all products
     for (const producthandle of productsHandles) {
       let title = "Null";
@@ -48,7 +78,7 @@ const stores = {"walmart":["https://www.walmart.ca/search?q=banana&c=10019", "di
           (path, el) => el.querySelector(path).textContent, stores[storename][2],
           producthandle
         );
-      } catch (error) {throw error}
+      } catch (error) { }
   
       try {
         //get price of single product
@@ -74,15 +104,15 @@ const stores = {"walmart":["https://www.walmart.ca/search?q=banana&c=10019", "di
     
       } catch (error) { }
     //put product into list
-    if (title !== "Null" && !price.includes("for")) {
+    if (title !== "Null" && !price.includes("for") && !price.includes("/") && img !== "Null") {
       if(price.includes("¢")){
         price = "$0." + price.slice(0, 2);
       }
-      if(producturl != "Null"){
+      if(producturl !== "Null"){
         producturl = "https://" + url.split("/")[2] + producturl;
       }
       fs.appendFile(
-        "products.csv",
+        `/Users/vincent/Documents/Projects/React/GroceryWebApp/grocery_webapp/src/website_search/${storename}.csv`,
         `${title.replace(/,/g, ".")},${price},${img},${producturl}\n`,
         function (err) {
           if (err) throw err;
@@ -92,8 +122,13 @@ const stores = {"walmart":["https://www.walmart.ca/search?q=banana&c=10019", "di
   }
   });
   for (const store in stores) {
-    await cluster.queue(stores[store][0]);
+    let correcturl = stores[store][0];
+    correcturl = correcturl.concat(`${object}`);
+    if(store == "walmart"){
+      correcturl = correcturl.concat("&c=10019");
+    }
+    await cluster.queue(correcturl);
   }
   await cluster.idle();
   await cluster.close();
-})();
+};
