fa9a18207a8909dec3202d4674878e8693c8657a
Dudemario <vincentjsun@gmail.com>
2023-07-29 22:53:33 -0400

    Update index.js

--- grocery_backend/index.js
+++ grocery_backend/index.js
@@ -10,37 +10,39 @@ puppeteer.use(StealthPlugin());
     headless: false,
     defaultViewport: false,
     userDataDir: "./tmp",
-    executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-    userDataDir: "/Users/vincent/Library/Application Support/Google/Chrome/Profile 2"
+    //executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    //userDataDir: "/Users/vincent/Library/Application Support/Google/Chrome/Profile 2"
   });
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
   await page.goto(
-    "https://www.walmart.ca/search?q=banana&c=10019"
+    "https://www.nofrills.ca/search?search-bar=banana"
   );
-  //await page.setGeolocation({ latitude: 43.4669, longitude: -79.6858 });
+  await page.waitForSelector('h3');
   const productsHandles = await page.$$(
-    "div.css-qsotkn.e5icx9n1 > div > .css-1d0izcz.e1m8uw9118"
+    "ul.product-tile-group__list.product-tile-group__list--total-48 > li.product-tile-group__list__item"
   );
 
   //loop through all products
   for (const producthandle of productsHandles) {
+    console.log("hi");
     let title = "Null";
     let price = "Null";
     let img = "Null";
+    let url = "Null";
 
     try {
       //get title of single product
       title = await page.evaluate(
-        (el) => el.querySelector("div > span > div > p").textContent,
+        (el) => el.querySelector("span.product-name__item.product-name__item--name").textContent,
         producthandle
       );
-    } catch (error) { }
+    } catch (error) {throw error}
 
     try {
       //get price of single product
       price = await page.evaluate(
-        (el) => el.querySelector("div > span > span").textContent,
+        (el) => el.querySelector("span.price__value.selling-price-list__item__price.selling-price-list__item__price--now-price__value").textContent,
         producthandle
       );
     } catch (error) { }
@@ -48,19 +50,27 @@ puppeteer.use(StealthPlugin());
   try {
     //get img of single product
     img = await page.evaluate(
-      (el) => el.querySelector(".css-19q6667.e175iya62").getAttribute("src"),
+      (el) => el.querySelector("div.product-tile__thumbnail__image > img").getAttribute("src"),
       producthandle
     );
-  } catch (error) { }
+  
+  } catch (error) {}
+  try {
+    //get img of single product
+    url = await page.evaluate(
+      (el) => el.querySelector("h3 > a").getAttribute("href"),
+      producthandle
+    );
 
+  } catch (error) { }
   //put product into list
-  if (title !== "Null" && !price.includes("for")) {
+  if (title !== "Null" && !price.includes("for")  && !price.includes("/")) {
     if(price.includes("¢")){
       price = "$0." + price.slice(0, 2)
     }
     fs.appendFile(
       "products.csv",
-      `${title.replace(/,/g, ".")},${price},${img}\n`,
+      `${title.replace(/,/g, ".")},${price},${img},www.metro.ca${url}\n`,
       function (err) {
         if (err) throw err;
       }
