22a556ea1a9dc5762bbb6a979442e0321ec300de
Benjamin Wang <benjaminwang42@gmail.com>
2023-07-30 15:10:51 -0400

    bug fix

--- grocery_webapp/src/components/Footer.js
+++ grocery_webapp/src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
             <LinkedInIcon />
           </a>
         </div>
-       <p> OptimalShopper.com test</p>
+       <p> OptimalShopper.com</p>
       </div>
     );
   }
